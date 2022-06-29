1ec4e74edf8b675c31685720a06b8e2807e02aac
Berton Quon <berton@ualberta.ca>
2022-06-29 11:59:48 -0600

    Add Bootstrap, add component template for card

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Card } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 class Cards extends Component {
   render() {
     return (
-      <div>
-        <card>
+      <div className="containter">
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>NAME HERE</Card.Title>
+            <Card.Text>
+              GPA HERE
+            </Card.Text>
+            <Button variant="primary">Grade</Button>
+          </Card.Body>
+        </Card>
 
-        </card>
-        Cards
     </div>
     )
   }
